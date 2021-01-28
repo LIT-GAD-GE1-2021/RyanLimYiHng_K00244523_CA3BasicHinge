d292250e842048660fa67fae4a53ba048cd6964a
LIT_Ryan <77435288+RyanLim1115@users.noreply.github.com>
2021-01-28 09:41:41 +0000

    task2

---
+++ Assets/Scripts/TrapdoorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HingeJoint2D))]
+
+public class TrapdoorScript : MonoBehaviour
+{
+    private HingeJoint2D hinge;
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        hinge = GetComponent<HingeJoint2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        hinge.useMotor = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
